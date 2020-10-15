a6abed29d0872d3880a0304b53e1a57e6b5f3474
cSDes1gn <christian911@sympatico.ca>
2020-10-15 00:55:00 -0400

    setup fixes

--- setup.sh
+++ setup.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-set -e
-
 OKGREEN='\033[92m'
 # WARNING='\033[93m'
 FAIL='\033[91m'
@@ -9,12 +7,14 @@ OKBLUE='\033[94m'
 UNDERLINE='\033[4m'
 ENDC='\033[0m'
 
-echo "Installing dependancies  ..."
-cat dependencies.txt | xargs -a brew install -y
+echo "Installing dependancies ..."
+cat dependencies.txt | xargs brew install
 echo -e "${OKGREEN} ✓ ${ENDC} complete"
 
-echo "Preparing usbboot submodule  ..."
+echo "Preparing usbboot submodule ..."
 git submodule update --init --recursive
-cd usboot
+echo "Syncing with usbboot remote"
+git submodule update --remote
+cd usbboot
 make
 echo -e "${OKGREEN} ✓ ${ENDC} complete"
